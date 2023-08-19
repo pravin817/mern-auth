ba096948cda40e813bf41fcc7830a83c752d46e1
Pravin Mhaske <pravinmhaske817@gmail.com>
2023-08-19 11:39:42 +0530

    Connect to DB

--- server/index.js
+++ server/index.js
@@ -1,7 +1,24 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+// intialise the dotenv
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to DB.");
+  })
+  .catch((error) => {
+    console.log("Error while connecting to database ... " + error);
+  });
 
 const app = express();
 
-app.listen(3000, (req,res)=>{
-    console.log("Server is running on port : 3000");
-})
+app.listen(3000, (req, res) => {
+  console.log("Server is running on port : 3000");
+});
