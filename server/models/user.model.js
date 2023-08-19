7d59a68cb3bc2cecfe20cc765abc6ad6ba7dc37d
Pravin Mhaske <pravinmhaske817@gmail.com>
2023-08-19 11:50:07 +0530

    Create user model

---
+++ server/models/user.model.js
@@ -0,0 +1,26 @@
+import mongoose, from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+
+  // it includes the creation and the updation time 
+},{timestamps:true});
+
+const User = mongoose.model('User',userSchema);
+
+//export the model
+export default User;
